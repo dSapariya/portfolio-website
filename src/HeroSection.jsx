7d52f63e2ccec1b64e706e26a09af6b3d2fe591b
dSapariya <dixasapariya786@gmail.com>
2025-04-04 13:28:51 +0530

    set first page mobile responsive

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { getImagePath } from "./utils/getImagePath";
-
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { SiLeetcode } from "react-icons/si";
 const HeroSection = () => {
 
   const scrollToSection = (id) => {
@@ -79,6 +80,42 @@ const HeroSection = () => {
               I'm a software developer, dedicated to mastering the latest technologies and design principles to build dynamic, responsive web solutions.
             </p>
           </div>
+        </div>
+      </div>
+      <div className='md:hidden'>
+        {/* Left Side Social Icons */}
+        <div className="absolute bottom-12 md:bottom-0 left-4 flex flex-col md:flex-col items-start md:items-center gap-3 z-50 text-white text-2xl">
+          {/* Icons: row on mobile, column on md+ */}
+          <div className="flex flex-row md:flex-col items-center gap-4">
+            <a href="https://www.linkedin.com/in/dixa-sapariya/" target="_blank" rel="noopener noreferrer">
+              <FaLinkedinIn className="hover:text-teal-400 cursor-pointer" />
+            </a>
+            <a href="https://github.com/dSapariya" target="_blank" rel="noopener noreferrer">
+              <FaGithub className="hover:text-teal-400 cursor-pointer" />
+            </a>
+            <a href="https://leetcode.com/u/dixa_sapariya/" target="_blank" rel="noopener noreferrer">
+              <SiLeetcode className="hover:text-teal-400 cursor-pointer" />
+            </a>
+          </div>
+
+          {/* Border line: hidden on mobile */}
+          <div className="hidden md:block md:h-24 border-l border-white z-10"></div>
+        </div>
+
+        {/* Right Side Email */}
+        <div className="absolute bottom-4 left-4 md:bottom-0 md:right-6 flex md:flex-col items-start md:items-end gap-3 z-50 text-white text-sm">
+          {/* Email: inline on mobile, vertical on md+ */}
+          <div className="flex flex-row md:flex-col items-center gap-4 md:mr-3">
+            <a
+              href="mailto:dixasapariya786@gmail.com"
+              className="hover:text-teal-400 tracking-wide md:rotate-90 md:origin-bottom-right cursor-pointer"
+            >
+              dixasapariya786@gmail.com
+            </a>
+          </div>
+
+          {/* Border line: hidden on mobile */}
+          <div className="hidden md:block h-24 border-l border-white z-10"></div>
         </div>
       </div>
     </div>
--- src/SocialBar.jsx
+++ src/SocialBar.jsx
@@ -4,9 +4,9 @@ import { SiLeetcode } from "react-icons/si";
 
 const SocialBar = () => {
     return (
-        <div>
+        <div className='hidden md:block'>
             {/* Left Side Social Icons */}
-            <div className="fixed bottom-12 left-4 flex flex-col md:flex-col items-start gap-3 z-50 text-white text-2xl">
+            <div className="fixed bottom-12 md:bottom-0 left-4 flex flex-col md:flex-col items-start md:items-center gap-3 z-50 text-white text-2xl">
                 {/* Icons: row on mobile, column on md+ */}
                 <div className="flex flex-row md:flex-col items-center gap-4">
                     <a href="https://www.linkedin.com/in/dixa-sapariya/" target="_blank" rel="noopener noreferrer">
@@ -21,13 +21,13 @@ const SocialBar = () => {
                 </div>
 
                 {/* Border line: hidden on mobile */}
-                <div className="hidden md:block h-24 border-l border-white z-10"></div>
+                <div className="hidden md:block md:h-24 border-l border-white z-10"></div>
             </div>
 
             {/* Right Side Email */}
-            <div className="fixed bottom-4 left-4 md:bottom-0 md:right-4 flex md:flex-col items-start md:items-end gap-3 z-50 text-white text-sm">
+            <div className="fixed bottom-4 left-4 md:bottom-0 md:right-6 flex md:flex-col items-start md:items-end gap-3 z-50 text-white text-sm">
                 {/* Email: inline on mobile, vertical on md+ */}
-                <div className="flex flex-row md:flex-col items-center gap-4">
+                <div className="flex flex-row md:flex-col items-center gap-4 md:mr-3">
                     <a
                         href="mailto:dixasapariya786@gmail.com"
                         className="hover:text-teal-400 tracking-wide md:rotate-90 md:origin-bottom-right cursor-pointer"
